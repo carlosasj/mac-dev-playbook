c179b90e10c601d1814735c46077538ddb4edeff
Carlos Schneider <carlosaschjr@gmail.com>
2025-08-22 16:56:40 +0100

    Karabiner Settings Checkpoint

--- make/make_sync_back.sh
+++ make/make_sync_back.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-[[ -f ~/.config/karabiner/karabiner.json ]] && cp ~/.config/karabiner/karabiner.json ../files/karabiner/karabiner.json
-[[ -f /etc/pam.d/sudo_local ]] && cp /etc/pam.d/sudo_local ../files/sudo/sudo_local
+[[ -f ~/.config/karabiner/karabiner.json ]] && cp ~/.config/karabiner/karabiner.json files/karabiner/
+[[ -f /etc/pam.d/sudo_local ]] && cp /etc/pam.d/sudo_local files/sudo/
