3543466ff26a65314e4641517f432c99eab9cf34
Carlos Schneider <carlosaschjr@gmail.com>
2025-08-22 21:15:22 +0100

    Update karabiner task to use symlink

--- make/make_sync_back.sh
+++ make/make_sync_back.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-[[ -f ~/.config/karabiner/karabiner.json ]] && cp ~/.config/karabiner/karabiner.json files/karabiner/
 [[ -f /etc/pam.d/sudo_local ]] && cp /etc/pam.d/sudo_local files/sudo/
